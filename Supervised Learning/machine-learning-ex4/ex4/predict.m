c9a02129ad42e3da440bf6e80499c11018b02806
Rohan Sukumaran <rohan.s16@iiits.in>
2017-08-12 11:20:04 +0530

    Add files via upload

---
+++ Supervised Learning/machine-learning-ex4/ex4/predict.m
@@ -0,0 +1,30 @@
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
+
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
+
+p = zeros(size(X, 1), 1);
+X = [ones(m, 1) X];
+
+
+z1=X*Theta1';
+predict = [ones(size(z1),1) sigmoid(z1)];
+z2=predict*Theta2';
+a3= sigmoid(z2);
+[predict_max, index_max] = max(a3, [], 2);
+p = index_max;
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+h1 = sigmoid([ones(m, 1) X] * Theta1');
+h2 = sigmoid([ones(m, 1) h1] * Theta2');
+[dummy, p] = max(h2, [], 2);
+
+% =========================================================================
+
+
+end
